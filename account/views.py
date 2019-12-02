e533746801c2609732ce71c72946c19ed55cd5d3
Naoki Hamanaka <naoki.hamanaka@edwardjapan.com>
2019-12-02 19:56:21 +0900

    Update views.py

--- account/views.py
+++ account/views.py
@@ -38,4 +38,4 @@ def login(request):
 def logout(request):
     if request.method == 'POST':
         auth.logout(request)
-        return render(request, 'accounts/signup.html')
+        return redirect('home')
