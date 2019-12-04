6def74b5a991e555720242c99a2eec326157207c
Naoki Hamanaka <naoki.hamanaka@edwardjapan.com>
2019-12-04 12:34:09 +0900

    Update models.py

--- company/models.py
+++ company/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    title = models.CharField(max_length=100)
+    companyname = models.CharField(max_length=20)
+    url = models.URLField()
+    image = ImageField()
+    evaluation = models.IntegerField(default=1)
