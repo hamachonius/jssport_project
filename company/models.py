ba1b44a5e8b89fca228e6c3d4aee7fcb56514234
Naoki Hamanaka <naoki.hamanaka@edwardjapan.com>
2019-12-04 17:03:45 +0900

    Update models.py

--- company/models.py
+++ company/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Company(models.Model):
-    title = models.CharField(max_length=100)
-    companyname = models.CharField(max_length=20)
-    url = models.URLField()
-    image = ImageField()
-    evaluation = models.IntegerField(default=1)
+    name = models.CharField(max_length=255)
+    summary = models.TextField()
+    location = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    votes_total = models.IntegerField(default=1)
+    companyid = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
