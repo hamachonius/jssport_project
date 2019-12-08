07420d30fd5996657b0a46e56e4b47ede260c368
Naoki Hamanaka <naoki.hamanaka@edwardjapan.com>
2019-12-08 17:38:05 +0900

    Update settings.py

--- jssport/settings.py
+++ jssport/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
 
