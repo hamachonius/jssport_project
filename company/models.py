7dc2a5851a8b2d479a89570fb94074e1ad723f2f
Naoki Hamanaka <naoki.hamanaka@edwardjapan.com>
2019-12-13 10:16:14 +0900

    forking errors

--- company/models.py
+++ company/models.py
@@ -3,17 +3,19 @@ from django.contrib.auth.models import User
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
-    summary_or_promotion = models.TextField()
+    introduction = models.TextField()
+    url = models.URLField()
     location = models.CharField(max_length=255)
     image = models.ImageField(upload_to='images/')
     evaluation = models.IntegerField(default=1)
+    regi_date = models.DateTimeField()
     companyid = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.name
 
     def summary(self):
         return self.body[:100]
 
     def pub_date_pretty(self):
-        return self.pub_date.strftime('%b %e %Y')
+        return self.regi_date.strftime('%b %e %Y')
--- company/views.py
+++ company/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Company
+from django.utils import timezone
 # Create your views here.
 def home(request):
     return render(request, 'company/home.html')
+
+@login_required
+def create (request):
+    if requested.method == 'POST':
+        if request.POST['name'] and request.POST['introduction'] and request.POST['url'] and request.FILES['image'] and request.POST['location']:
+           company = Company()
+           company.name = request.POST['name']
+           company.introduction = request.POST['introduction']
+           if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+               company.url = request.POST['url']
+           else:
+               company.url = 'http://' + request.POST['url']
+           company.image = request.FILES['image']
+           company.regi_date = timezone.datetime.now()
+           company.companyid = request.user
+           company.save()
+           return redirect('home')
+        else:
+           return render(request, 'company/create.html',{'error':'All fields are required.'})
+    else:
+        return render(request, 'company/registercompany.html')
