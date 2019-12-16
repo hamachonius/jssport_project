9f085e10d402c1921fca2fd9fb352d81af654178
Naoki Hamanaka <naoki.hamanaka@edwardjapan.com>
2019-12-16 12:40:34 +0900

    Update models.py

--- jishusei/models.py
+++ jishusei/models.py
@@ -6,6 +6,14 @@ class Jishusei(models.Model):
     introduction = models.TextField()
     edu_background_duration_y = models.IntegerField()
     edu_background_duration_m = models.IntegerFields()
-
     carrer_background = models.TextField()
     profile_pic = models.ImageField(upload_to='profile_pic/')
+
+    def __str__(self):
+        return self.name
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.regi_date.strftime('%b %e %Y')
