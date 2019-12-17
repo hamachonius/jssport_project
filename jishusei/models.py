27b5cf66fa220a9359c7d8a4a6e15255eff0485d
YuichiTaura <e1321244@gmail.com>
2019-12-18 03:23:02 +0900

    Update models.py

--- jishusei/models.py
+++ jishusei/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Jishusei(models.Model):
+    name = models.CharField(max_length=50)
+    introduction = models.TextField()
+    edu_background_duration_y = models.IntegerField()
+    edu_background_duration_m = models.IntegerField()
+    carrer_background = models.TextField()
+    profile_pic = models.ImageField(upload_to='profile_pic/')
+
+    def __str__(self):
+        return self.name
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.regi_date.strftime('%b %e %Y')
